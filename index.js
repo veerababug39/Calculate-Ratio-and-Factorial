635bd42bff74a6c0002fc0087dd9bcfd9fc33151
veerababu <veerababug39@gmail.com>
2023-05-02 09:18:03 +0000

    Calculate Ratio and Factorial solution

---
+++ index.js
@@ -0,0 +1,9 @@
+const ratioOfTwoNumbers = require("../ratio/index.js");
+const factorialOfNumber = require("../factorial/index.js");
+
+const ratioAndFactorial = (num1, num2, num3) => {
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  const factorial = factorialOfNumber(num3);
+  return { ratio, factorial };
+};
+module.exports = ratioAndFactorial;
